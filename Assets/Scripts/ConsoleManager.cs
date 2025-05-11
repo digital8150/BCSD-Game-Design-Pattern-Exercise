4d3e0459a9c2ab9f2df463c735bd94454f31fd7d
Digital <73296205+digital8150@users.noreply.github.com>
2025-05-12 02:17:37 +0900

    SingleTone 구현

---
+++ Assets/Scripts/ConsoleManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ConsoleManager : SingleTone<ConsoleManager>
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("ConsoleManager µÓ¿Â!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
